67ffe7c1480f50dfe6a53e43f410b69c978214db
ngb4o <ngbao08052003@gmail.com>
2024-08-30 17:50:11 +0700

    Firebase setup commit

---
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -0,0 +1,12 @@
+
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  // Variables
+  final deviceStorage = GetStorage();
+
+  // Called from main.dart on app launch
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app.dart';
 import 'firebase_options.dart';
@@ -10,15 +11,15 @@ Future<void> main() async {
   // Add Widgets Binding
   final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
-  // Todo: Init Local Storage
+  // GetX Local Storage
+  await GetStorage.init();
 
-  // Todo: Init Payment Methods
-
-  // Todo: Await Native Splash
+  // Await Native Splash until other items Load
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Todo: Initialize Firebase
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()))
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
 
   // Todo: Initialize Authentication
 
