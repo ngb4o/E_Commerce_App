604d4c8da0a8fb85dff786faefa44db41a70d242
haoking1111 <ngbao08052003@gmail.com>
2024-08-30 12:11:42 +0700

    firebase setup commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
+
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  // Add Widgets Binding
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Todo: Init Local Storage
+
+  // Todo: Init Payment Methods
+
+  // Todo: Await Native Splash
+
+  // Todo: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()))
+
+  // Todo: Initialize Authentication
+
+  // Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
